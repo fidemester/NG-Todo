76ff2c54958af407f71e3c121c7f39fba9e01d54
Krisztian <fodorkrisztian6@gmail.com>
2021-05-08 21:57:00 +0200

    "ng 08-ig"

---
+++ src/app/todo.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Todo} from "./todo";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  static API_URL = "http://localhost:3000/todos";
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  postTodo(todo: Todo): Observable<any> {
+    return this.http.post(TodoService.API_URL, todo)
+  }
+
+  getAllTodos(): Observable<any> {
+    return this.http.get(TodoService.API_URL)
+  }
+
+  deleteTodo(id:number): Observable<any> {
+    return this.http.delete(TodoService.API_URL+'/'+`${id}`)
+  }
+}
